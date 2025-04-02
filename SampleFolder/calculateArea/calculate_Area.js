bad956bf9b9f385e20fc908f570cac67ea100b04
waspJelly <wasp.jelly@proton.me>
2025-04-02 15:00:16 -0400

    updated files with the grocery calculator

--- SampleFolder/calculateArea/calculate_Area.js
+++ SampleFolder/calculateArea/calculate_Area.js
@@ -1,12 +1,14 @@
-let length;
-let width;
+let grocery1;
+let grocery2;
+let grocery3;
 
-function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
+function calculateTotalAmount() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
    
-let area = length * width;
+let total = grocery1 + grocery2 + grocery3;
 
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+document.getElementById('result').innerText = `The total amount is:$ ${total}`;
 }
 
