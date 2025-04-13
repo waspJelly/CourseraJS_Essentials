6ca64c238127f69f3670c6b07979d161b0346c57
waspJelly <wasp.jelly@proton.me>
2025-04-13 16:20:01 -0400

    updated the files to include employee specialization

--- SampleFolder/employeeDetails/employee_details.js
+++ SampleFolder/employeeDetails/employee_details.js
@@ -1,12 +1,12 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, speciaization: 'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, speciaization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, speciaization: 'Java' },
     //... More employee records can be added here
     ];
 
  // Function to display all employees
-const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.speciaization}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 //   }
 
@@ -28,4 +28,13 @@ function findEmployeeById(employeeId) {
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }}
+
+function findBySpecializationJS() {
+    const foundSpecialization = employees.find(employee => employee.speciaization === 'Javascript');
+    if (foundSpecialization) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundSpecialization.id}: ${foundSpecialization.name} - ${foundSpecialization.department} - ${foundSpecialization.speciaization}</p>`;
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }}
